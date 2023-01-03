788aca16dcb59b1d684ece09be0e8443f6b3382d
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-03 19:50:20 +0330

    create constants taht include some unique ids

---
+++ shared/constants.ts
@@ -0,0 +1,20 @@
+export const SEED_ARTISTS = [
+  "45YMrIBH74j8e2wNlRSSdK",
+  "3QNGoF6VzVNnkpjJDT3NHq",
+  "3JS9sHeI06RtolBR5s5O0L",
+  "578d6XzxI0bCSi49dK0Qpx",
+  "0jCVTRvQkILbJvpviTpvd1",
+].join(",");
+
+export const TOP_PLAYLISTS = [
+  "6Zh3Qz6tvsHRoI6EvxEEQu",
+  "37i9dQZF1DXcBWIGoYBM5M",
+  "37i9dQZEVXbMDoHDwVN2tF",
+  "4hOKQuZbraPDIfaGbM3lKI",
+  "1KNl4AYfgZtOVm9KHkhPTF",
+  "5iwkYfnHAGMEFLiHFFGnP4",
+  "37i9dQZEVXbLRQDuF5jeBp",
+  "37i9dQZF1DX7Jl5KP2eZaS",
+  "37i9dQZF1DX18jTM2l2fJY",
+  "4BDcwKv9RSKKJ1bCVgfhNn",
+];
