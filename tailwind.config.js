4f598e0b1715f9d5536ecd3937da76a44e24c5b6
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-03 19:47:41 +0330

    refine tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: "#12181B",
+        "dark-hovered": "#1b2428",
+      },
+      keyframes: {
+        "fade-out": {
+          from: {
+            opacity: 1,
+          },
+          to: {
+            opacity: 0,
+          },
+        },
+        "fade-in": {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 }
