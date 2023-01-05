c0571de9921a361acc5f7e62287937a93d31f2d0
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-05 13:35:58 +0330

    create a player context

---
+++ context/PlayerContext.tsx
@@ -0,0 +1,33 @@
+import { FC, createContext, useState } from "react";
+
+export const PlayerContext = createContext<{
+  playerId: string;
+  setPlayerId: Function;
+  isPlayerIdChanged: boolean;
+  setIsPlayerIdChanged: Function;
+}>({
+  playerId: "",
+  setPlayerId: () => {},
+  isPlayerIdChanged: false,
+  setIsPlayerIdChanged: () => {},
+});
+
+export const PlayerContextProvider: FC<{ children: any }> = ({ children }) => {
+  const [playerId, setPlayerId] = useState("");
+  const [isPlayerIdChanged, setIsPlayerIdChanged] = useState(false);
+
+  console.log(playerId);
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        playerId,
+        setPlayerId,
+        isPlayerIdChanged,
+        setIsPlayerIdChanged,
+      }}
+    >
+      {children}
+    </PlayerContext.Provider>
+  );
+};
