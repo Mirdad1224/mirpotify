8cbce915ccb88b76aa82f21850eec41a6f684835
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-05 13:44:37 +0330

    clear some logs and redundent data

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -13,26 +13,18 @@ const Layout = ({ children }: { children: JSX.Element }) => {
   const [loadingState, setLoadingState] = useState(LoadingStates.loading);
 
   const { playerId } = useContext(PlayerContext);
-  console.log(
-    `Basic ${Buffer.from(
-      `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`
-    ).toString("base64")}`
-  );
-  console.log(process.env.NEXT_PUBLIC_FINAL);
+
   useEffect(() => {
     localStorage.setItem("mirpotify-playing", playerId);
   }, [playerId]);
+
   useEffect(() => {
-    // const data = new URLSearchParams()
-    // data.append('grant_type','client_credentials')
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
     myHeaders.append(
       "Authorization",
       `Basic ${process.env.NEXT_PUBLIC_FINAL?.toString()}`
     );
-    // myHeaders.append("Cookie", "__Host-device_id=AQBCZnpBGImv3mJt8W85k2bUxiA6TgsdxL__34W464Yxm6wi_EoeOppiLB4a9Gv2kHDwTDUM2ZtxKTZQRSXqgg06C-w17rkBU6g");
-
     const urlencoded = new URLSearchParams();
     urlencoded.append("grant_type", "client_credentials");
 
@@ -45,42 +37,14 @@ const Layout = ({ children }: { children: JSX.Element }) => {
     fetch("https://accounts.spotify.com/api/token", requestOptions)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         if (data.access_token) {
           setLoadingState(LoadingStates.finished);
           client.setAccessToken(data.access_token);
         } else setLoadingState(LoadingStates.error);
       })
-      .catch((err) => {
-        console.log(err);
+      .catch((_err) => {
         setLoadingState(LoadingStates.error);
       });
-    // var urlencoded = new URLSearchParams();
-    // urlencoded.append("grant_type", "client_credentials");
-    // fetch("https://accounts.spotify.com/api/token", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/x-www-form-urlencoded",
-    //     Authorization:
-    //       "Basic " +
-    //       Buffer.from(
-    //         process.env.CLIENT_ID + ":" + process.env.CLIENT_SECRET
-    //       ).toString("base64"),
-    //   },
-    //   body: urlencoded,
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //     if (data.access_token) {
-    //       setLoadingState(LoadingStates.finished);
-    //       client.setAccessToken(data.access_token);
-    //     } else setLoadingState(LoadingStates.error);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     setLoadingState(LoadingStates.error);
-    //   });
   }, []);
 
   if (loadingState === LoadingStates.loading)
@@ -97,7 +61,6 @@ const Layout = ({ children }: { children: JSX.Element }) => {
   return (
     <>
       <Navbar />
-
       <div className="min-h-[calc(100vh-144px)]">{children}</div>
       {!!playerId && <Player key={playerId} />}
     </>
