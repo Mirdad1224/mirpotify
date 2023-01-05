fadd7bca224c8c9e0c8a2f51d7ae8ac9d4bc46e0
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-05 13:42:11 +0330

    provide context and layout for app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { PlayerContextProvider } from "../context/PlayerContext";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PlayerContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </PlayerContextProvider>
+  );
 }
